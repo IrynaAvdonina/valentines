4b9a32579d38a4a3ebea96b20c6f5c9ab425b455
Iryna <avdoninairin@gmail.com>
2025-02-11 20:54:07 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import { useState, useEffect, MouseEvent } from "react";
+import "./App.css";
+
+function ValentineBackground({ type }: { type: "hearts" | "gradient" })
+{
+  useEffect(() =>
+  {
+    if (type === "hearts")
+    {
+      const interval = setInterval(() =>
+      {
+        const heart = document.createElement("div");
+        heart.className = "heart";
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+        document.body.appendChild(heart);
+        setTimeout(() => heart.remove(), 5000);
+      }, 300);
+      return () =>
+      {
+        clearInterval(interval);
+        document.querySelectorAll(".heart").forEach(el => el.remove());
+
+      }
+    }
+  }, [type]);
+
+  return <div className={`background ${type}`}></div>;
+}
+
+export default function ValentinePage()
+{
+  const [accepted, setAccepted] = useState(false);
+  const [noClicked, setNoClicked] = useState(0);
+  const [noPosition, setNoPosition] = useState({ top: "50%", left: "50%" });
+  const [isMoving, setIsMoving] = useState(false);
+
+  const handleYesClick = () =>
+  {
+    setAccepted(true);
+    const audio = new Audio("/src/assets/valentines.mp3");
+    audio.loop = true;
+    audio.play();
+  };
+
+  const moveNoButton = () =>
+  {
+    const randomX = Math.random() * 80 + "%";
+    const randomY = Math.random() * 80 + "%";
+    setNoPosition({ top: randomY, left: randomX });
+  };
+
+  const handleNoClick = (e: MouseEvent) =>
+  {
+    setNoClicked(prev => prev + 1);
+    setIsMoving(true);
+    (e.target as HTMLButtonElement).disabled = true;
+    moveNoButton();
+  };
+
+  const handleNoHover = () =>
+  {
+    if (isMoving)
+    {
+      moveNoButton();
+    }
+  };
+
+  return (
+    <>
+      {accepted ? (
+        <ValentineBackground type="hearts" />
+      ) : (
+        <ValentineBackground type="gradient" />)}
+      <div className="container">
+        {accepted ? (
+          <div>
+            <h1 className="love-text"> Ура! Кохаю тебе! 💖</h1>
+            <img src="/src/assets/happy.gif" />
+          </div>
+        ) : (
+          <div>
+            <h1 className="question-text">Чи станеш ти моїм Валентином?</h1>
+            {noClicked === 0 ? (
+              <div className="heart-shape">
+                <img
+                  src="/src/assets/shy.gif"
+                  alt="cute cat"
+                  className="gif"
+                />
+              </div>) : (
+              <img
+                src="/src/assets/sad.jpg"
+                alt="sad cat"
+                width={"240px"}
+                height={"240px"}
+                className="sad-img"
+              />
+            )}
+            <div className="button-container">
+              <button onClick={handleYesClick} className="yes-button">
+                Так 💘
+              </button>
+              <div onMouseEnter={handleNoHover}
+                style={isMoving ? { top: noPosition.top, left: noPosition.left, position: "absolute" } : {}}
+              >
+                <button onClick={handleNoClick} className="no-button">
+                  Ні
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+      </div>
+    </>
+  );
+}
